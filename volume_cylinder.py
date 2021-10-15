eb4e6e59a4fafe20a066a24a2370b590fb59f5db
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-15 17:10:07 +0000

    volume of a cylinder 2

--- volume_cylinder.py
+++ volume_cylinder.py
@@ -2,13 +2,13 @@
 
 # Created by: Haokai Li
 # Created on: Oct 2021
-# This Program calculate the volume of a cylinder
+# This Program calculates the volume of a cylinder
 
 import math
 
 
 def calculate_volume_cylinder(radius, height):
-    # This function calculate the volume of a cylinder
+    # This function calculates the volume of a cylinder
 
     # process
     volume = radius ** 2 * height * math.pi
@@ -18,6 +18,10 @@ def calculate_volume_cylinder(radius, height):
 
 def main():
     # This function just call other functions
+
+    print("This Program calculates the volume of a cylinder.")
+    print("Please enter the radius and height.")
+    print("")
 
     # input
     user_radius_string = input("Please enter the radius of a cylinder(cm): ")
