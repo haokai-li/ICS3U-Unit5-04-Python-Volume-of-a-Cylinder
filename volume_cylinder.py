c0e2ae1c70adee28b1e46d3190cd4fb9da4379c8
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-15 16:25:42 +0000

    volume of a cylinder

--- volume_cylinder.py
+++ volume_cylinder.py
@@ -35,7 +35,7 @@ def main():
 
         # output
         print(
-            "The volume of the cylinder is {} cm².".format(calculated_volume_cylinder)
+            "The volume of the cylinder is {} cm³.".format(calculated_volume_cylinder)
         )
 
     except Exception:
